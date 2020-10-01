10028bea0c191f0bffefe1909126777049c79621
sahil-ra <sahilsingh0847@gmail.com>
2020-10-01 19:53:26 +0530

    Update ColorHandler.java

--- src/main/java/ColorHandler.java
+++ src/main/java/ColorHandler.java
@@ -1,9 +1,13 @@
+//COLOR CHANGED
 public class ColorHandler {
     static int[] getColor(String key){
         int color[] = new int[3];
         color[0] = (Integer.parseInt(key.substring(12)) + Integer.parseInt(key.substring(0,4)))%256;
         color[0] = (Integer.parseInt(key.substring(12)) + Integer.parseInt(key.substring(4,8)))%256;
         color[0] = (Integer.parseInt(key.substring(12)) + Integer.parseInt(key.substring(8,12)))%256;
+        int y = 0;
         return color;
     }
 }
+
+// THIS IS A CHANGE BY SAHIL !!
